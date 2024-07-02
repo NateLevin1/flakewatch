db40711ca7b06d2baa2fc0d94175e9c3f8de4d71
NateLevin1 <63370104+NateLevin1@users.noreply.github.com>
2024-07-02 15:45:22 -0400

    🐛 Make project directory

--- backend/flakewatch.ts
+++ backend/flakewatch.ts
@@ -28,6 +28,9 @@ flakewatch(projectInfo);
 
 export async function flakewatch(project: ProjectInfo) {
     let result = {} as FlakewatchResults;
+    await fs.mkdir("/home/flakewatch/clone/" + project.name, {
+        recursive: true,
+    });
     try {
         // * Update the project to the latest commit
         try {
