c1eb926bf5e6afb81c84b53d62c214f54761bc3b
NateLevin1 <63370104+NateLevin1@users.noreply.github.com>
2024-07-17 13:43:08 -0400

    🐛 Close database on SIGINT

--- orchestration/db.ts
+++ orchestration/db.ts
@@ -24,6 +24,11 @@ export function setup() {
     db.prepare(
         "CREATE TABLE IF NOT EXISTS projects (name TEXT PRIMARY KEY, lastCheckedCommit TEXT)"
     ).run();
+
+    process.on("exit", () => db.close());
+    process.on("SIGHUP", () => process.exit(128 + 1));
+    process.on("SIGINT", () => process.exit(128 + 2));
+    process.on("SIGTERM", () => process.exit(128 + 15));
 }
 
 export function toCsv(fn: () => Flaky[]) {
