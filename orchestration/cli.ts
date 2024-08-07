2d7dedae32b7bd4c327276a30ec7615f57115736
NateLevin1 <63370104+NateLevin1@users.noreply.github.com>
2024-08-07 17:45:13 -0400

    ✨ CLI: Remove container if already exists

--- orchestration/cli.ts
+++ orchestration/cli.ts
@@ -10,7 +10,13 @@ export async function cli(args: string[]) {
         console.log(
             "Usage: npm start -- <gitURL> <commit> <test> (module) (--keep-alive)"
         );
-        return;
+        process.exit(1);
+    }
+    if (!test.includes("#")) {
+        console.log(
+            "Test must be in the format <testPackage>.<testClass>#<testMethod>"
+        );
+        process.exit(1);
     }
 
     gitURL = gitURL.replace(".git", "").replace(/\/$/, "");
@@ -33,12 +39,17 @@ export async function cli(args: string[]) {
         },
     };
 
+    const containerName = `flakewatch-${project.name}-${
+        test.split("#")[1]
+    }-${commit.slice(0, 7)}`;
+    await exec(`docker rm -f ${containerName}`);
+
     const cloneCmd = `cd /home/flakewatch/clone/ && git clone ${gitURL} ${name} && cd ${name} && git checkout ${commit}`;
     const updateFlakewatchCmd = `cd /home/flakewatch/flakewatch/backend && git pull && npm install && npm run build`;
     const projectJson = JSON.stringify(project).replaceAll('"', '\\"');
     const cmd = `/bin/bash -c "${cloneCmd} && ${updateFlakewatchCmd} && npm run detect-from-test -- '${projectJson}'"`;
     await exec(
-        `docker run --name='flakewatch-${project.name}' -i flakewatch:base ${cmd}`
+        `docker run --name='${containerName}' -i flakewatch:base ${cmd}`
     );
     await readFlakewatchResultsToDB(project as unknown as ProjectInfo);
 }
